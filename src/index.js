9855d9fedee2333576cb9a2ea17e445068335161
evanszymkowicz <evan.szymkowicz@evanwolf.co>
2019-01-31 12:01:43 -0500

    Semantic UI styling

--- src/index.js
+++ src/index.js
@@ -2,12 +2,41 @@ import React from "react";
 import ReactDom from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Are you sure?</h4>
+        </div>
+      </ApprovalCard>
       {/*Components are exempt from needing curly braces*/}
-      <CommentDetail />
+      <ApprovalCard>
+      <CommentDetail
+        author="Nathan"
+        timeAgo="Today at 4:45PM"
+        content="True"
+        avatar={faker.image.avatar()}
+      />
+      </ApprovalCard>
+      <ApprovalCard>
+      <CommentDetail
+        author="Isaac"
+        timeAgo="Yesterday at 11:35PM"
+        content="I didn't read it"
+        avatar={faker.image.avatar()}
+      />
+      </ApprovalCard>
+      <ApprovalCard>
+      <CommentDetail
+        author="Adam"
+        timeAgo="Thursday at 9:03AM"
+        content="this is an offhand comment"
+        avatar={faker.image.avatar()}
+      />
+      </ApprovalCard>
     </div>
   );
 };
